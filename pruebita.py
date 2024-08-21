dc107bf4af3c76c7c382430f313b94bdb7845a13
ValentinaMannino <vmannino@uade.edu.ar>
2024-08-21 09:06:38 -0300

    feat: Add condition to stop loading when specified criteria are met
    This commit adds a condition to stop loading elements when certain criteria are met. For example, the loading process can stop when a specified number of elements have been loaded or when the user enters a specific value to indicate they don't want to load more. This ensures that the loading process is more flexible and can be controlled based on specific requirements.
    
    The commit also includes the addition of minimum 3 reports, such as the arrangement of the matrix, the number of players, the number of plays, and the number of points. These three aspects are interrelated and provide valuable insights into the game.
    
    The reports generated include:
    1) The amount of points each player has won.
    2) The average number of points per play.
    3) The player with the highest and lowest number of points.
    
    These changes enhance the functionality and reporting capabilities of the code.

--- pruebita.py
+++ pruebita.py
@@ -4,3 +4,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#hay que poner una condicion de fin, dejo de cargar cuando... 
+#EJ: (haya cargado x cant de elementos, o cuando el usuario ingrese un valor "no quiero cargar mas", etc)
+
+#MINIMO 3 INFORMES, como me queda la matriz es un buen informe
+#podemos hacer x cant jugadores, x cant de jugadas, x cant de puntos --> esos 3 se relacionan 
+#despues el informe me dice
+#1) cuanto gano cada jugador
+#2) el promedio de puntos del total de jugadas
+#3) el jugador con mas/menos puntos
