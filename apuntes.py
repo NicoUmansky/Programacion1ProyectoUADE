3f8b945a6e3ab818f41179f316d731bf25e7bd98
Micaela Cembal <47297659@est.ort.edu.ar>
2024-09-04 08:47:58 -0300

    Funciones Programa Principal

---
+++ apuntes.py
@@ -0,0 +1,17 @@
+
+#OBLIGATORIO: TEMA - ALCANCE - INFORMES (3)
+
+#hay que poner una condicion de fin, dejo de cargar cuando... 
+#EJ: (haya cargado x cant de elementos, o cuando el usuario ingrese un valor "no quiero cargar mas", etc)
+
+#MINIMO 3 INFORMES, como me queda la matriz es un buen informe
+#podemos hacer x cant jugadores, x cant de jugadas, x cant de puntos --> esos 3 se relacionan 
+#despues el informe me dice
+#1) cuanto gano cada jugador
+#2) el promedio de puntos del total de jugadas
+#3) el jugador con mas/menos puntos
+#otra opcion: listado ordenado... 
+
+#puedo poner un log in (dos listas paralelas- nombre de usuario + contraseña)- 
+#aca incorporo cadena de caracteres y modulo. 
+#Funcion que los valide- requerimiento (usuario valido segun nuestras condiciones y que el usuario no se repita)
