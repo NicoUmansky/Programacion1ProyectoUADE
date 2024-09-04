5d06c3c8b348316359fa8bdfa4358bfe5322a684
nicogiorda <ngiordano@uade.edu.ar>
2024-09-04 08:36:21 -0300

    feat: Add function to handle penalty kicks
    This commit adds a new function `penal()` to handle penalty kicks in the game. The function prompts the user to choose a target area to kick the ball. The target areas are represented by numbers from 1 to 9, arranged in a grid format.
    
    This change enhances the gameplay experience by introducing penalty kicks as an additional feature.

---
+++ Penales.py
@@ -0,0 +1,12 @@
+
+    
+#Funcion para patear un penal en caso de que la respuesta sea correcta
+def penal():
+    print("Es momento de patear un penal para tener la posibilidad de sumar puntos extra, elige a donde quieres patear:")
+    print("┎⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯┒")
+    print("|  1  ┆  2  ┆  3  |")
+    print("|  4  ┆  5  ┆  6  |")
+    print("|  7  ┆  8  ┆  9  |")
+    casilla = int(input("Ingresa el número de la casilla a la que quieres patear: "))
+
+penal()
