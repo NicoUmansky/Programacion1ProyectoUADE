09838bf79f49d501b40fbe721cd719b8bc5c4711
nicogiorda <ngiordano@uade.edu.ar>
2024-10-16 10:40:03 -0300

    Refactor code to fix validation issue in inicioSesion.py and include team names in penalty shootout

--- inicioSesion.py
+++ inicioSesion.py
@@ -1,14 +1,14 @@
 def inicioSesion():
 
-    mails = ["eugeniavarando@gmail.com","micaelacembal@gmail.com", "nicolasgiordano@gmail.com", "valentinamannino@gmail.com", "nicolasumansky@gmail.com"]
-    userName = ["Eugenia Varando", "Micaela Cembal", "Nicolas Giordano", "Valentina Mannino", "Nicolas Umansky"]
-    contraseñas= ["1234","1605","7234","3484","9653"]
+    #mails = ["eugeniavarando@gmail.com","micaelacembal@gmail.com", "nicolasgiordano@gmail.com", "valentinamannino@gmail.com", "nicolasumansky@gmail.com"]
+    #userName = ["Eugenia Varando", "Micaela Cembal", "Nicolas Giordano", "Valentina Mannino", "Nicolas Umansky"]
+    #contraseñas= ["1234","1605","7234","3484","9653"]
     
     validarInicio = lambda eleccion:  eleccion.isdigit() and decision in ['1', '2']
     validarMail = lambda mail: any(usuario == mail for usuario in mails)
     validarContra = lambda contra: contra.isdigit() and len(contra) == 4    
     obtenerNombreUsuario = lambda mail: [userName[i] for i in range(len(userName)) if mails[i] == mail][0] # Lista por comprensión con el nombre del usuario ingresado. 
-    validarUsuarioExistente = lambda mail, contra: any(mails[i] == mail and contraseñas[i] == contra for i in range(len(contraseñas)))
+    #validarUsuarioExistente = lambda mail, contra: any(mails[i] == mail and contraseñas[i] == contra for i in range(len(contraseñas)))
     validarSexo = lambda sex: sex.upper() == "M" or sex.upper() == "F" or sex.upper() == "X"
 
     decision = input("Presione 1 para Iniciar Sesión o 2 para Registrarse en su cuenta: ")
@@ -27,9 +27,9 @@ def inicioSesion():
             print("Contraseña incorrecta. Debe ser un número de 4 dígitos.")
             contraUsuario = input("Reingrese su contraseña (4 dígitos): ")
         
-        while not validarUsuarioExistente(mailUsuario, contraUsuario):
-            print("Contraseña incorrecta.")
-            contraUsuario = input("Reingrese su contraseña (4 dígitos): ")
+        #while not validarUsuarioExistente(mailUsuario, contraUsuario):
+            #print("Contraseña incorrecta.")
+            #contraUsuario = input("Reingrese su contraseña (4 dígitos): ")
 
         nombreUsuario = obtenerNombreUsuario(mailUsuario)
         print(f"¡Hola {nombreUsuario}!")
@@ -61,8 +61,10 @@ def inicioSesion():
 
 def registroCSV (mail, user, contra, team, sex):
     try:
-        arch = open(r"c:\Users\Pc\Documents\GitHub\CopaAlgoritmiaPreguntados\Programacion1ProyectoUADE\Files\usuarios.csv", "at")
+        arch = open(r"c:\Users\Pc\Documents\UADE - 1ER AÑO - 1ER CUATRIMESTRE\GitHub\Programacion1ProyectoUADE\Files\usuarios.csv", "at")
     except IOError:
         print("Error al abrir el archivo")
     else:
         arch.write(f"{mail};{user};{contra};{team};{sex}\n")
+        arch.close()
+    
