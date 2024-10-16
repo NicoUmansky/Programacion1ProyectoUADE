a7504af5b73644b24ebf1ad03e1ae8637346138c
Micaela Cembal <47297659@est.ort.edu.ar>
2024-10-16 11:06:33 -0300

    Preguntas a diccionario

---
+++ pruebaPreguntas.py
@@ -0,0 +1,30 @@
+rutaArchivo = '/Users/micaelacembal/Desktop/UADE/Programación I/Programacion1ProyectoUADE/Files/preguntas.txt'
+preguntas = {}
+
+try:
+    with open(rutaArchivo, 'r') as file:
+        lines = file.readlines()
+        
+        for line in lines:
+            partes = line.strip().split(';')
+            numeroPregunta = int(partes[0].strip())  
+            pregunta = partes[1].strip()              
+            opciones = [option.strip() for option in partes[2].split(',')]  
+            respuestaCorrecta = int(partes[3].strip())   
+            
+            preguntas[numeroPregunta] = {
+                "pregunta": pregunta,
+                "opciones": opciones,
+                "respuestaCorrecta": respuestaCorrecta
+            }
+
+except FileNotFoundError:
+    print("Error: No se pudo encontrar el archivo de preguntas.")
+
+print(preguntas[1])
+print("---------------------")
+print(preguntas[1]["pregunta"])
+print("---------------------")
+print(preguntas[1]["opciones"])
+print("---------------------")
+print(preguntas[1]["respuestaCorrecta"])
