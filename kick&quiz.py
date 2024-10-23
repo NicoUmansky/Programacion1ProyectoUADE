acca643c05c34ed41881535853ae923da5b44017
Micaela Cembal <47297659@est.ort.edu.ar>
2024-10-23 10:49:17 -0300

    AgregarPregunta en Kick&Quiz OK

--- kick&quiz.py
+++ kick&quiz.py
@@ -1,7 +1,7 @@
 rutaArchivo1 = "Programacion1ProyectoUADE/Files/preguntas.txt" 
 rutaArchivo2 = "Files\preguntas.txt"
 
-rutaElegida = rutaArchivo2
+rutaElegida = rutaArchivo1
 
 from triviaPreguntas import jugarPreguntas
 from ranking import mostrarRanking, actualizarRanking, puntuaciones
@@ -57,7 +57,46 @@ def cargarPreguntas(rutaElegida):
     
     return preguntas, opciones, respuestasCorrectas
 
+def agregarPregunta(rutaElegida):
+    
+        print(yellow + "Agregar Nueva Pregunta:" + reset)
+        pregunta = input("Ingrese la pregunta: ")
+    
+        opciones = []
+        for i in range(4):
+            opcion = input(f"Ingrese la opción de respuesta {i + 1}: ")
+            opciones.append(opcion)
 
+        respuestaCorrecta = input("Ingrese el número de la opción correcta (1-4): ")
+        while not (respuestaCorrecta.isdigit() and int(respuestaCorrecta) in [1, 2, 3, 4]):
+            print(f"{yellow}Opción inválida. Debe ser un número entre 1 y 4.{reset}")
+            respuestaCorrecta = input("Ingrese el número de la opción correcta (1-4): ")
+
+        respuestaCorrecta = int(respuestaCorrecta) - 1
+        archivoPreguntas = open(rutaElegida, 'a', encoding='utf-8')
+        numeroPregunta = obtenerNumeroPregunta(rutaElegida)
+        opcionesTexto = ','.join(opciones)
+
+        archivoPreguntas.write(f"{numeroPregunta};{pregunta};{opcionesTexto};{respuestaCorrecta}\n")
+        archivoPreguntas.close() 
+        print(yellow + "¡Pregunta agregada exitosamente!" + reset)
+
+def obtenerNumeroPregunta(rutaElegida):
+    #Consigo el numero de la ultima pregunta para asignarle el siguiente numero a la que estoy creando
+    try:
+        archivoPreguntas = open(rutaElegida, 'r', encoding='utf-8')
+        lineas = archivoPreguntas.readlines()
+        archivoPreguntas.close()
+        if lineas:
+            ultimoLinea = lineas[-1].strip()
+            return int(ultimoLinea.split(';')[0]) + 1
+        else:
+            return 1 #Si el archivo esta vacio porque no hay preguntas, le asigno el indice 1
+        
+    except FileNotFoundError:
+        print("Error: No se pudo encontrar el archivo de preguntas.")
+        return 0
+    
 def main():
 
     continuar = True
@@ -99,7 +138,7 @@ def main():
             mostrarRanking()
     
         elif opcion == '3': 
-            print(red + "¡Agregar pregunta! ¡Próximamente!")
+            agregarPregunta(rutaElegida)
             
         elif opcion == '4': 
             print(red + "¡Muchas gracias por jugar! Te esperamos nuevamente.")
